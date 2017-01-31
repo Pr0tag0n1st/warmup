b15b89305f9a082a70ab6fc670e409bc50a5122f
Pr0tag0n1st <665825@dpsk12.net>
2017-01-31 07:50:36 -0700

    asd

--- jan31/jan31/Source.cpp
+++ jan31/jan31/Source.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main() {
 	cout << "Why are calendars so rare?" << endl << endl << endl << endl << endl << endl << endl << endl << endl;
-//add a pause
+//add a pauses
 	cout << "Because their days are numbered!" << endl;
 }
