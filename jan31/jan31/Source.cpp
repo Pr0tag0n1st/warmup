2cdd7989a480df3fdc7d8a075a84493b016b6056
Pr0tag0n1st <665825@dpsk12.net>
2017-01-31 07:49:03 -0700

    asdasd

---
+++ jan31/jan31/Source.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Why are calendars so rare?" << endl << endl << endl << endl << endl << endl << endl << endl << endl;
+
+	cout << "Because their days are numbered!" << endl;
+}
